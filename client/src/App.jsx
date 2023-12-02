6d5e334f2fa4221d3f9b0b612cf9ba78e2b2636e
Diyana Cholakova <didkacholakova999@gmail.com>
2023-12-02 11:50:54 +0200

    added login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Footer from "./components/footer/Footer"
 import Header from "./components/header/Header"
 import Home from "./components/Home/Home"
+import Login from './components/login/Login'
 import RecipeAdd from './components/recipe-add/RecipeAdd'
 import RecipeList from './components/recipe-list/RecipeList'
 
@@ -17,6 +18,7 @@ function App() {
                 <Route path='/' element={<Home />} />
                 <Route path='/recipes' element={<RecipeList />} />
                 <Route path='/recipes/add' element={<RecipeAdd />} />
+                <Route path='/login' element={<Login />} />
             </Routes>
         </main>
 
---
+++ client/src/components/login/Login.jsx
@@ -0,0 +1,35 @@
+export default function Login() {
+    return(
+        <div className="content-wrap login">
+            <h2 className="page-title divider">Log in</h2>
+
+            <section className="form-section">
+
+                <form id="login-form" method="post">
+
+                    <fieldset>
+
+                        <legend>Login</legend>
+
+                        <div className="form-group">
+                            <label htmlFor="email" className="form-label">Email</label>
+                            <input type="email" id="email" name="email" className="form-control" />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="password" className="form-label">Password</label>
+                            <input type="password" id="password" name="password" className="form-control" />
+                        </div>
+
+                        <div>
+                            <button id="login-submit" type="submit" className="sbm-btn">Log in</button>
+                        </div>
+                    </fieldset>
+
+                    <div className="login-register-switch">Don't have an account? Go to <a link="/register">register</a>.</div>
+                </form>
+
+            </section>
+        </div>
+    );
+}
