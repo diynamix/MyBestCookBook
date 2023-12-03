a4453efa7d9dd92647e4e2961f1d490291e79bf3
Diyana Cholakova <didkacholakova999@gmail.com>
2023-12-03 21:12:15 +0200

    RecipeAdd - controlled form

--- client/src/components/recipe-add/RecipeAdd.jsx
+++ client/src/components/recipe-add/RecipeAdd.jsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import * as recipeService from "../../services/recipeService";
 
+const formInitialState = {
+    name: '',
+    imageUrl: '',
+    ingredients: '',
+    steps: '',
+};
+
 export default function RecipeAdd() {
+    const [formValues, setFormValues] = useState(formInitialState);
     const navigate = useNavigate();
+
+    const changeHandler = (e) => {
+        setFormValues(state => ({
+            ...state,
+            [e.target.name]: e.target.value,
+        }));
+    };
+    
+    const resetFormHandler = () => {
+        setFormValues(formInitialState);
+    };
 
     const addRecipeSubmitHandler = async (e) => {
         e.preventDefault();
 
-        const recipeData = Object.fromEntries(new FormData(e.currentTarget));
+        // const recipeData = Object.fromEntries(new FormData(e.currentTarget));
         
         try {
-            await recipeService.add(recipeData);
+            await recipeService.add(formValues);
 
             navigate('/recipes');
         } catch (error) {
@@ -19,7 +39,7 @@ export default function RecipeAdd() {
             console.log(error);
         }
     }
-
+    
     return (
         <div className="content-wrap add-recipe">
             <h2 className="page-title divider">Add</h2>
@@ -32,26 +52,56 @@ export default function RecipeAdd() {
 
                         <div className="form-group">
                             <label htmlFor="name" className="required">Recipe title</label>
-                            <input type="text" id="name" name="name" placeholder="Recipe title" required />
+                            <input
+                                type="text"
+                                id="name"
+                                name="name"
+                                value={formValues.name}
+                                onChange={changeHandler}
+                                placeholder="Recipe title"
+                                required />
                         </div>
 
                         <div className="form-group">
                             <label htmlFor="imageUrl" className="required">Image link</label>
-                            <input type="text" id="imageUrl" name="imageUrl" placeholder="Image link" required />
+                            <input
+                                type="text"
+                                id="imageUrl"
+                                name="imageUrl"
+                                value={formValues.imageUrl}
+                                onChange={changeHandler}
+                                placeholder="Image link"
+                                required />
                         </div>
 
                         <div className="form-group">
                             <label htmlFor="ingredients" className="required">Ingredients</label>
-                            <textarea id="ingredients" name="ingredients" rows="7" placeholder="Ingredients..." required></textarea>
+                            <textarea
+                                id="ingredients"
+                                name="ingredients"
+                                rows="7"
+                                value={formValues.ingredients}
+                                onChange={changeHandler}
+                                placeholder="Ingredients..."
+                                required>
+                            </textarea>
                         </div>
 
                         <div className="form-group">
                             <label htmlFor="steps" className="required">Steps</label>
-                            <textarea id="steps" name="steps" rows="7" placeholder="Steps..." required></textarea>
+                            <textarea
+                                id="steps"
+                                name="steps"
+                                rows="7"
+                                value={formValues.steps}
+                                onChange={changeHandler}
+                                placeholder="Steps..."
+                                required>
+                            </textarea>
                         </div>
 
                         <div>
-                            <input className="sbm-btn" type="submit" value="Add recipe" />
+                            <input className="button sbm-btn" type="submit" value="Add recipe" />
                         </div>
 
                     </fieldset>
